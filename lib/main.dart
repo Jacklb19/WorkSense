93eb06e50b796c94c7f635b4f783dae44a234fa7
Santiago Arevalo <santif.arevalo@gmail.com>
2026-04-21 15:56:23 -0500

    Initialize project structure and add dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'dart:io';
 
 import 'core/routing/app_router.dart';
 import 'core/theme/app_theme.dart';
